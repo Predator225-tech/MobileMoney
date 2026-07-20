be292282a1923fc944c0c53f2bb89404ea475fc1
Herimamy Fitia Mandresy <fitiamandresyh@gmail.com>
2026-07-20 12:35:01 +0300

    refactorisation pages login

--- MobileMoney/app/Config/Routes.php
+++ MobileMoney/app/Config/Routes.php
@@ -4,39 +4,42 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 
-$routes->group('admin', function($routes) {
-    
-    // Dashboard et Gains
-    $routes->get('dashboard', 'Operator\OperatorController::index');
-    $routes->get('gains', 'Operator\OperatorController::showGains');
-    $routes->get('operations', 'Operator\OperationController::index');
-    $routes->post('operations/store', 'Operator\OperationController::store');
-    $routes->post('operations/update/(:num)', 'Operator\OperationController::update/$1');
-    $routes->get('operations/delete/(:num)', 'Operator\OperationController::delete/$1');
+// 1. La page d'accueil (URL "/") redirige vers le formulaire de login
+$routes->get('/', 'Client\AuthController::index');
 
-    // Gestion des Préfixes (tout est sous Operator)
-    $routes->get('prefixes', 'Operator\PrefixeController::index');
-    $routes->post('prefixes/store', 'Operator\PrefixeController::store');
-    $routes->get('prefixes/delete/(:num)', 'Operator\PrefixeController::delete/$1');
-
-    // Gestion des barèmes de frais
-    $routes->get('baremes', 'Operator\BaremeController::index');
-    $routes->get('baremes/(:num)', 'Operator\BaremeController::index/$1');
-    $routes->post('baremes/store', 'Operator\BaremeController::store');
-    $routes->post('baremes/update/(:num)', 'Operator\BaremeController::update/$1');
-    $routes->get('baremes/delete/(:num)', 'Operator\BaremeController::delete/$1');
-    
-});
-
-$routes->group('auth', function($routes) {
-    $routes->get('/', 'Client\AuthController::index');
+// 2. Groupe Authentification
+$routes->group('auth', function ($routes) {
     $routes->post('login', 'Client\AuthController::login');
     $routes->get('logout', 'Client\AuthController::logout');
 });
 
-$routes->group('client', function($routes) {
-    $routes->get('dashboard', 'Client\DashboardController::index');
-    $routes->post('deposit', 'Client\DashboardController::deposit');
-    $routes->post('withdraw', 'Client\DashboardController::withdraw');
-    $routes->post('transfer', 'Client\DashboardController::transfer');
+// 3. Groupe Admin
+$routes->group('admin', function ($routes) {
+    $routes->get('dashboard', 'Operator\OperatorController::index');
+    $routes->get('gains', 'Operator\OperatorController::showGains');
+
+    // --- Gestion des Préfixes ---
+    $routes->get('prefixes', 'Operator\PrefixeController::index');
+    $routes->post('prefixes/store', 'Operator\PrefixeController::store');
+    $routes->get('prefixes/delete/(:num)', 'Operator\PrefixeController::delete/$1');
+
+    // --- Gestion des Types d'Opérations (AJOUTÉ ICI) ---
+    $routes->get('types-operations', 'Operator\OperationController::index');
+    $routes->post('types-operations/store', 'Operator\OperationController::store');
+    $routes->post('types-operations/update/(:num)', 'Operator\OperationController::update/$1');
+    $routes->get('types-operations/delete/(:num)', 'Operator\OperationController::delete/$1');
+
+    // --- Gestion des barèmes de frais ---
+    $routes->get('baremes', 'Operator\BaremeController::index');
+    $routes->post('baremes/store', 'Operator\BaremeController::store');
+    $routes->post('baremes/update/(:num)', 'Operator\BaremeController::update/$1');
+    $routes->get('baremes/delete/(:num)', 'Operator\BaremeController::delete/$1');
+});
+
+// 4. Groupe Client
+$routes->group('client', ['namespace' => 'App\Controllers\Client'], function($routes) {
+    $routes->get('dashboard', 'DashboardController::index');
+    $routes->post('deposit', 'DashboardController::deposit');
+    $routes->post('withdraw', 'DashboardController::withdraw');
+    $routes->post('transfer', 'DashboardController::transfer');
 });
